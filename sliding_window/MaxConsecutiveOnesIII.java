cbfef24506feccba1ffe78eb03b6675d438c5c40
zuzukobaladze <60299995+zuzukobaladze@users.noreply.github.com>
2025-01-08 14:32:09 +0400

    Remove main again

--- sliding_window/MaxConsecutiveOnesIII.java
+++ sliding_window/MaxConsecutiveOnesIII.java
@@ -1,9 +1,4 @@
 public class MaxConsecutiveOnesIII {
-	public static void main(String[] args){
-		System.out.println(longestOnes(new int[]{0,0,1,1,0,0,1,1,1}, 3));
-		System.out.println(longestOnes(new int[] {1,1,1,0,0,0,1,1,1,1,0}, 2));
-
-	}
 
 	public static int longestOnes(int[] nums, int k) {
 		int maxLen = 0;
