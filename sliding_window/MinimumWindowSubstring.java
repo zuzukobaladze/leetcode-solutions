065b184ae4b80ac9b8468d46c034a5e5bee70d87
zuzukobaladze <60299995+zuzukobaladze@users.noreply.github.com>
2025-01-11 12:48:55 +0400

    Syntax

--- sliding_window/MinimumWindowSubstring.java
+++ sliding_window/MinimumWindowSubstring.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class MinimumWindowSubstring {
-	public static void main(String[] args){
 	public static String minWindow(String s, String t){
 		String word = new String(s);
 		int l1 = s.length();
