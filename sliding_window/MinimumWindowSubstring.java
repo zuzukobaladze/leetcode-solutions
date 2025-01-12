bf386603ef27723b51684f9d0c6fa328e77164b4
zuzukobaladze <60299995+zuzukobaladze@users.noreply.github.com>
2025-01-12 17:14:40 +0400

    Minimum window substring progress

--- sliding_window/MinimumWindowSubstring.java
+++ sliding_window/MinimumWindowSubstring.java
@@ -1,19 +1,16 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class MinimumWindowSubstring {
 	public static String minWindow(String s, String t){
-		String word = new String(s);
+		String word = "";
 		int l1 = s.length();
 		int l2 = t.length();
 		int matches = 0;
 		int nextIndex = 0;
 		HashMap<Character, Integer> targetMap = new HashMap<>();
-		HashSet<Character> targetSet = new HashSet<>();
 
 		for (char c  : t.toCharArray()) {
 			targetMap.merge(c, 1, Integer::sum);
-			targetSet.add(c);
 		}
 
 		for (int j = 0, i = 0; i < l1; i++) {
@@ -22,21 +19,6 @@ public class MinimumWindowSubstring {
 				targetMap.compute(right, (k, v) -> v - 1);
 				if(targetMap.get(right) == 0) matches++;
 			}
-			else if(matches == l2){
-				if(word.length() > i - j + 1){
-					word = s.substring(j, i);
-				}
-				char left = s.charAt(j);
-				if (targetSet.contains(left)) {
-					targetMap.merge(left, 1, Integer::sum);
-					matches--;
-				}
-				j = nextIndex;
-			}
-			else{
-				nextIndex++;
-			}
-			System.out.println(targetMap);
 		}
 
 		return word;
