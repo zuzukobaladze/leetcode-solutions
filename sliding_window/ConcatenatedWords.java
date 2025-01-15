6b6e8f543424cda29119b97b069d5739665a048f
zuzukobaladze <60299995+zuzukobaladze@users.noreply.github.com>
2025-01-15 15:00:13 +0400

    Concatenated words start

--- sliding_window/ConcatenatedWords.java
+++ sliding_window/ConcatenatedWords.java
@@ -1,11 +1,16 @@
-import java.util.List;
+import java.util.*;
 
 public class ConcatenatedWords {
   public static void main(String[] args) {
-
+    
   }
 
   public List<String> findAllConcatenatedWordsInADict(String[] words) {
-    
+    HashMap<String, Integer> wordMap = new HashMap<>();
+    for (String word : words) {
+      wordMap.merge(word, 1, Integer::sum);
+    }
+    System.out.println(wordMap);
+    return null;
   }
 }
