8c821261dbcdb0a915139e2d4b189557b13da634
Zurabi Kobaladze <zkobaladze002@gmail.com>
2025-01-27 23:46:07 +0400

    Two Sum done with HashMap

--- two_pointers/TwoSum.java
+++ two_pointers/TwoSum.java
@@ -1,11 +1,18 @@
-class TwoSum {
-  public static void main(String[] args){
-    
-  }
+import java.util.HashMap;
 
-    public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-          
+class TwoSum {
+    //Two Sum with HashMap
+    public static int[] twoSumMap(int[] nums, int target) {
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+      }
+      for (int i = 0; i < nums.length; i++) {
+        Integer j = map.get(target - nums[i]);
+        if(j != null && j != i){
+          return new int[]{i, j};
         }
+      }
+        return new int[]{};
     }
 }
