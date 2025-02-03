f0dc2f03770c6442ed201de10efca3b25583d97d
Zurabi Kobaladze <zkobaladze002@gmail.com>
2025-02-03 22:32:40 +0400

    Fix

--- two_pointers/SquaresOfASortedArray.java
+++ two_pointers/SquaresOfASortedArray.java
@@ -2,12 +2,7 @@
 import java.util.Arrays;
 
 public class SquaresOfASortedArray {
-  public static void main(String[] args) {
-    int[] res1 = sortedSquares(new int[] { -12, -6, -4, -1, 2, 3, 5, 7, 8, 10 });
-    int[] res2 = sortedSquares(new int[] {0, 3, 5, 7, 8 });
-    System.out.println(Arrays.toString(res1));
-    System.out.println(Arrays.toString(res2));
-  }
+
 
   public static int[] sortedSquares(int[] nums) {
     int[] res = new int[nums.length];
