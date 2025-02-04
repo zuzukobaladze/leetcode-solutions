7a3460d78e7c4ec0f732449cda2b76b7aeb2309a
Zurabi Kobaladze <zkobaladze002@gmail.com>
2025-02-04 19:16:45 +0400

    Changes

--- two_pointers/RemoveDuplicates.java
+++ two_pointers/RemoveDuplicates.java
@@ -1,9 +1,6 @@
 
 
 public class RemoveDuplicates {
-  public static void main(String[] args) {
-    removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
-  }
 
   public static int removeDuplicates(int[] nums) {
     int k = 1;
