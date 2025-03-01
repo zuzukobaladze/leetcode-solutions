fdf2ce5f2f8872d1e084b94d8e1cb7785cf2abd5
Zurabi Kobaladze <zkobaladze002@gmail.com>
2025-03-01 16:35:16 +0400

    Changed

--- sliding_window/MaximumSumofDistinctSubarraysWithLengthK.java
+++ sliding_window/MaximumSumofDistinctSubarraysWithLengthK.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 public class MaximumSumofDistinctSubarraysWithLengthK {
 
-  public static void main(String[] args) {
-    int[] nums1 = {1,5,4,2,9,9,9};
-    int[] nums2 = {1,1,1,7,8,9};
-    int[] nums3 = {5,3,3,1,1};
-    System.out.println(maximumSubarraySum(nums1, 3));
-    System.out.println(maximumSubarraySum(nums2, 3));
-    System.out.println(maximumSubarraySum(nums3, 3));
-  }
-
   public static long maximumSubarraySum(int[] nums, int k) {
     long max = 0;
     long cur = 0;
